7ec669b5860e408fd331a236e3944918590170bb
Gerard Hickey <Gerard.Hickey@smartsheet.com>
2018-04-22 21:35:42 -0700

    Basic files

---
+++ depalma.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from flask import Flask
+from flask_github import GitHub
+
+app = Flask(__name__)
+app.config['GITHUB_CLIENT_ID'] = 'hickey'
+app.config['GITHUB_CLIENT_SECRET'] = 'c4b338f565bb627bab0a9c2faf994a3bbc72a54f'
+
+# For GitHub Enterprise
+app.config['GITHUB_BASE_URL'] = 'https://api.github.com/api/v3/'
+app.config['GITHUB_AUTH_URL'] = 'https://github.com/login/oauth/'
+
+github = GitHub(app)
