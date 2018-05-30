39f20d8ff6ad2a9d2aa30916cc0c48507d5926c2
Gerard Hickey <Gerard.Hickey@smartsheet.com>
2018-05-30 00:50:04 -0700

    Update depalma.py

--- depalma.py
+++ depalma.py
@@ -5,7 +5,7 @@ from flask_github import GitHub
 
 app = Flask(__name__)
 app.config['GITHUB_CLIENT_ID'] = 'hickey'
-app.config['GITHUB_CLIENT_SECRET'] = 'c4b338f565bb627bab0a9c2faf994a3bbc72a54f'
+app.config['GITHUB_CLIENT_SECRET'] = '04b338f565bb627bab0a9c2faf994a3bbc72a54f'
 
 # For GitHub Enterprise
 app.config['GITHUB_BASE_URL'] = 'https://api.github.com/api/v3/'
